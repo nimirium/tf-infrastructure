768202dd422f41c1da21c0c968fffacdc455c8ca
Sophie Fogel <sophie@sensibo.com>
2022-01-06 16:22:09 +0200

    added network acls

--- vpc.tf
+++ vpc.tf
@@ -99,6 +99,206 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+resource "aws_network_acl" "public_acl" {
+  vpc_id = aws_vpc.main_vpc.id
+  subnet_ids = [aws_subnet.public_subnet.id]
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80  # http
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443  # https
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22  # ssh
+    to_port    = 22
+  }
+
+  # Allows inbound return traffic from hosts on the internet that are responding to requests originating in the subnet.
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 400
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  ingress {  # deny all other inbound traffic
+    protocol   = "tcp"
+    rule_no    = 500
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80  # http
+    to_port    = 80
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443  # https
+    to_port    = 443
+  }
+
+  egress {  # Allows outbound MongoDB access to DB in private subnet
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = aws_subnet.private_subnet.cidr_block
+    from_port  = 27017  # mongo
+    to_port    = 27017
+  }
+
+  egress {  # Allows outbound responses to clients on the internet
+    protocol   = "tcp"
+    rule_no    = 400
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 32768
+    to_port    = 65535
+  }
+
+  egress {  # Allows outbound SSH access to instances in your private subnet
+    protocol   = "tcp"
+    rule_no    = 500
+    action     = "allow"
+    cidr_block = aws_subnet.private_subnet.cidr_block
+    from_port  = 22  # ssh
+    to_port    = 22
+  }
+
+  egress {  # Denies all outbound IPv4 traffic not already handled by a preceding
+    protocol   = "all"
+    rule_no    = 9999
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0  # ssh
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = var.project_name
+  }
+}
+
+resource "aws_network_acl" "private_acl" {
+  vpc_id = aws_vpc.main_vpc.id
+  subnet_ids = [aws_subnet.private_subnet.id]
+
+  ingress {  # allow mongo access from public subnet
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_subnet.public_subnet.cidr_block
+    from_port  = 27017  # mongodb
+    to_port    = 27017
+  }
+
+  ingress {  # deny mongo access any other place
+    protocol   = "tcp"
+    rule_no    = 150
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 27017  # mongodb
+    to_port    = 27017
+  }
+
+  ingress {  # allow ssh access from public subnet
+    protocol   = "200"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = aws_subnet.public_subnet.cidr_block
+    from_port  = 22  # ssh
+    to_port    = 22
+  }
+
+  # Allows inbound return traffic from the NAT device in the public subnet for requests originating in the private subnet
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  ingress {  # don't allow other inbound traffic
+    protocol   = "all"
+    rule_no    = 9999
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  egress {  # Allows outbound responses to the public subnet
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = aws_subnet.public_subnet.cidr_block
+    from_port  = 32768
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "all"
+    rule_no    = 9999
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = var.project_name
+  }
+}
+
 resource "aws_instance" "web_server" {
   ami = var.basic_ami
   subnet_id = aws_subnet.public_subnet.id
